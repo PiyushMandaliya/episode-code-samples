a539ef8837646e504b51f050fcac2c7660e1e044
Piyush Mandaliya <mandaliyapiyush77@gmail.com>
2025-07-05 12:34:03 -0400

    Exercise 11

--- 0011-composition-without-operators/CompositionWithoutOperators.playground/Pages/02-Exercises.xcplaygroundpage/Contents.swift
+++ 0011-composition-without-operators/CompositionWithoutOperators.playground/Pages/02-Exercises.xcplaygroundpage/Contents.swift
@@ -3,12 +3,35 @@
 
  1. Write concat for functions (inout A) -> Void.
  */
-// TODO
+func concat<A>(_ fs: ((inout A) -> Void)...) -> (inout A) -> Void {
+  return { a in
+    for f in fs {
+      f(&a)
+    }
+  }
+}
+
 /*:
  2. Write concat for functions (A) -> A.
  */
-// TODO
+func concat<A>(_ f: ((A) -> A)...) -> (A) -> A {
+  return { a in
+    f.reduce(a) { value, f in
+      f(value)
+    }
+  }
+}
 /*:
  3. Write compose for backward composition. Recreate some of the examples from our functional setters episodes (part 1 and part 2) using compose and pipe.
  */
-// TODO
+func compose<A, B, C>(_ f: @escaping (B) -> C, _ g: @escaping (A) -> B) -> (A) -> C {
+  return { a in
+    f(g(a))
+  }
+}
+
+func pipe<A, B, C>(_ f: @escaping (A) -> B, _ g: @escaping (B) -> C) -> (A) -> C {
+  return { a in
+    g(f(a))
+  }
+}
